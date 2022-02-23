ffc92527d41be811f2921a88185e73b9242c0757
Navya <kbnavya@gmail.com>
2022-02-23 22:25:30 +0530

    commit

---
+++ gitdemowork/src/gitdemowork/gitclass.java
@@ -0,0 +1,12 @@
+package gitdemowork;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi i am in git");
+	}
+
+}
