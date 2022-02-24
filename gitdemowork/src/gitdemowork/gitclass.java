63e6a07261d42c9e3cea9d059d95c5caef1fca9f
Navya <kbnavya@gmail.com>
2022-02-24 11:13:19 +0530

    commit1change code

--- gitdemowork/src/gitdemowork/gitclass.java
+++ gitdemowork/src/gitdemowork/gitclass.java
@@ -5,6 +5,12 @@ public class gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
 		
 		System.out.println("Hi i am in git");
 	}
